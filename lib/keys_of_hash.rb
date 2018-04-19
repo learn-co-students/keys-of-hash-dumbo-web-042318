e5534677c146913edfccb77bf6a60eb296907320
jcr2125 <email>
2018-04-19 15:48:48 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,6 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    self.find_all{ |key, value| arguments.include?(value) }.flatten.select.each_with_index { |element, i| i.even? }
   end
 end
